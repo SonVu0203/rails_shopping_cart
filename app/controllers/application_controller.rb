777190333204fd1ef4f282260b3493df2e229285
Vu Van Son <vuvanson0203@gmail.com>
2020-03-18 14:11:53 +0700

    fish add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include SessionsCustomersHelper
+
+  before_action :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find_by(:id => session[:cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:cart_id] = nil
+      end
+    end
+
+    if session[:cart_id].nil?
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  end
 end
---
+++ app/models/cart_item.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :bigint           not null, primary key
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  cart_id    :bigint           not null
+#  product_id :bigint           not null
+#
+# Indexes
+#
+#  index_cart_items_on_cart_id     (cart_id)
+#  index_cart_items_on_product_id  (product_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cart_id => carts.id)
+#  fk_rails_...  (product_id => products.id)
+#
+
+class CartItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :cart
+
+  # validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
+
+  def total_price
+    self.quantity * self.product.price
+  end
+
+end
